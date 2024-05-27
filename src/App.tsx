8615b30a15360d4446f760730f84f354fb3387d9
Appmaniazar-Projects <appmaniazar@gmail.com>
2024-05-27 16:20:08 +0200

    Added firebase to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
+import React, { useEffect } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home/Home';
 import { Menu } from './components/Menu/Menu';
-
-
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -24,29 +23,39 @@ import '@ionic/react/css/typography.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import Events from './pages/Events/Events'
+import Events from './pages/Events/Events';
 import LineUp from './pages/LineUp/LineUp';
 import Profiles from './pages/Profiles/Profiles';
 import SocialMedia from './pages/SocialMedia/SocialMedia';
-
+import profileData from './pages/Profiles/profile'; // adjust import based on your file structure
+import { weeklySchedule } from './pages/LineUp/data'; // adjust import based on your file structure
+import { events } from './pages/Events/event'; // adjust import based on your file structure
+import { uploadProfiles, uploadEvents, uploadSchedule } from './services/dataService';
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <Menu />
-      <IonRouterOutlet id="main">
-        <Route path="/home" component={Home} exact={true} />
-        <Route path="/Events" component={Events} exact={true} />
-        <Route path="/LineUp" component={LineUp} exact={true} />
-        <Route path="/Profiles" component={Profiles} exact={true} />
-        <Route path="/SocialMedia" component={SocialMedia} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
-    </IonReactRouter>
-   
-  </IonApp>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    uploadProfiles(profileData);
+    uploadEvents(events);
+    uploadSchedule(weeklySchedule);
+  }, []);
+
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <Menu />
+        <IonRouterOutlet id="main">
+          <Route path="/home" component={Home} exact={true} />
+          <Route path="/Events" component={Events} exact={true} />
+          <Route path="/LineUp" component={LineUp} exact={true} />
+          <Route path="/Profiles" component={Profiles} exact={true} />
+          <Route path="/SocialMedia" component={SocialMedia} exact={true} />
+          <Route exact path="/" render={() => <Redirect to="/home" />} />
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;
